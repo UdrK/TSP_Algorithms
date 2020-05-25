b94f26e34febab79eccbf6ae0a0147390d9d1d51
UdrK <nomefittizioqualunque@gmail.com>
2020-05-25 16:19:24 +0200

    better main for testing

--- main.py
+++ main.py
@@ -8,17 +8,31 @@ directory_name = 'tsp_dataset'
 tot = len(os.listdir(directory_name))
 for file in os.listdir(directory_name):
     filename = os.fsdecode(file)
-    if filename.endswith("202.tsp"):
+    if filename.endswith(".tsp"):
         file_path = os.path.join(directory_name, filename)
-        print("Reading: {}".format(file_path))
+        print("Processing: {}".format(file_path))
+
         gr = txt_reader.file_to_graph(file_path)
         alg = Algorithms()
-        beginning = time.time()
-        #res = alg.HK_TSP(gr)
-        res = alg.two_approx_TSP(gr)
-        ending = time.time()
-        res2 = alg.nearest_neighbor_heuristic_TSP(gr)
-        ending2 = time.time()
-        print("two approx time: {} res: {}".format(ending - beginning, res))
-        print("nearest neighbor time: {} res: {}".format(ending2 - ending, res2))
 
+        beginning = time.time()
+        exact_res = alg.HK_TSP(gr, 1)
+        ending = time.time()
+
+        heuristic_res = alg.nearest_neighbor_heuristic_TSP(gr)
+        ending2 = time.time()
+
+        two_approx_res = alg.two_approx_TSP(gr)
+        ending3 = time.time()
+
+        f_exact = open('exact_results.txt', 'a+')
+        f_exact.write('{}, {}, {}\n'.format(filename, ending - beginning, exact_res))
+        f_exact.close()
+
+        f_heuristic = open('heuristic_results.txt', 'a+')
+        f_heuristic.write('{}, {}, {}\n'.format(filename, ending - ending2, heuristic_res))
+        f_heuristic.close()
+
+        f_two_approx = open('two_approx_results.txt', 'a+')
+        f_two_approx.write('{}, {}, {}\n'.format(filename, ending3 - ending2, two_approx_res))
+        f_two_approx.close()
